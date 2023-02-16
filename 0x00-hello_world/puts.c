9ae30a650c52315a572c638e0039ab0d3a6d6434
Ismailbouzarhoun <Ismailbouzarhoune09@gmail.com>
2023-02-16 20:50:28 +0000

    a

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -A program that prints line with puts function
+ * return: 0(success)
+ */
+int main(void)
+{
+	puts("\"Programming is like buiding a multilingual puzzle");
+	return (0);
+}
+
